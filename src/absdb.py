09fb28193589856ce46a9031e4baf1c59cec0c86
CHRISTIAN R. BENITEZ MARTINEZ <christianr.benitezmartinez@192.168.1.130>
2024-10-28 17:07:03 -0500

    adding cardanop

--- src/absdb.py
+++ src/absdb.py
@@ -65,7 +65,6 @@ def calcula_tendencia(_limit) :
             connection.close()
             print("MySQL connection is closed")
 
-
 def calcula_tendencia_ethereum(_limit) :
     try:
         # Conectar a la base de datos MySQL
@@ -139,6 +138,62 @@ def calcula_tendencia_solana(_limit) :
                                         WITH ultimos_registros AS (
                                         SELECT `daily-operation-price`
                                             FROM db.`daily-operation_solana`
+                                            ORDER BY `daily-operation-date` DESC
+                                            LIMIT {_limit}
+                                        ),
+                                        promedio AS (
+                                            SELECT AVG(`daily-operation-price`) AS promedio_venta
+                                            FROM ultimos_registros
+                                        )
+                                        SELECT 
+                                            SUM(CASE WHEN ur.`daily-operation-price` > p.promedio_venta THEN 1 ELSE 0 END) AS arriba_del_promedio,
+                                            SUM(CASE WHEN ur.`daily-operation-price` < p.promedio_venta THEN 1 ELSE 0 END) AS abajo_del_promedio
+                                        FROM ultimos_registros ur, promedio p;
+                                    """
+
+
+
+            # Ejecutar la sentencia SQL
+            cursor.execute(sql_insert_query)
+
+            resultados = cursor.fetchone()
+            arriba_del_promedio, abajo_del_promedio = resultados
+
+            #sacamos el promedio de los valores cuando _limit = 100%
+
+            arriba_del_promedio = (arriba_del_promedio/_limit)*100
+            abajo_del_promedio = (abajo_del_promedio/_limit)*100
+
+
+            return arriba_del_promedio, abajo_del_promedio
+
+    
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        # Cerrar la conexión a la base de datos
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+def calcula_tendencia_cardano(_limit) :
+    try:
+        # Conectar a la base de datos MySQL
+        connection = mysql.connector.connect(
+            host='localhost',  # Cambia esto si tu contenedor está en otra dirección
+            database='db',
+            user='user',
+            password='password'
+        )
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            # Crear la sentencia SQL
+            sql_insert_query = f"""
+                                        WITH ultimos_registros AS (
+                                        SELECT `daily-operation-price`
+                                            FROM db.`daily-operation_cardano`
                                             ORDER BY `daily-operation-date` DESC
                                             LIMIT {_limit}
                                         ),
--- src/cardano.py
+++ src/cardano.py
@@ -3,13 +3,13 @@ import mysql.connector
 from mysql.connector import Error
 from datetime import datetime
 from messages import manda_mail
-from absdb import calcula_tendencia_solana
+from absdb import calcula_tendencia_cardano
 
 currency = 'mxn' #sacar de las variables de env  
-crypto = 'bitcoin,Ripple,ethereum,dogecoin,solana' #ethereum
-valor_compra = 3374.18 # valor de tolerencia antes de vender - oportunidad de compra
-valor_inicial_compra = 3530.09
-valor_inversion_inicial = 2485.39
+crypto = 'cardano'
+valor_compra = 5.0 # valor de tolerencia antes de vender - oportunidad de compra    
+valor_inicial_compra = 6.7030
+valor_inversion_inicial = 474.83 #lo que tienes en pesos mexicanos a la hora de la compra
 porcentaje_tolerancia_min = 4.5 # sirve pasa saber cuando comprar 1328000 y 4.5 = 1268240.0
 porcentaje_ganacia_ideal = 5
 
@@ -27,7 +27,7 @@ def insert_record(fecha, valor):
                 cursor = connection.cursor()
 
                 # Crear la sentencia SQL
-                sql_insert_query = """INSERT INTO db.`daily-operation_solana` (`daily-operation-date`, `daily-operation-price`) VALUES (%s, %s)"""
+                sql_insert_query = """INSERT INTO db.`daily-operation_cardano` (`daily-operation-date`, `daily-operation-price`) VALUES (%s, %s)"""
 
                 # Datos a insertar
                 record_to_insert = (fecha, valor)
@@ -61,21 +61,21 @@ response = requests.get(url)
 if response.status_code == 200:
     # Convertir la respuesta JSON a un diccionario de Python
     data = response.json()
-    # Obtener el precio del cripto_price en USD
-    cripto_price = data['solana'][currency]
-    print(f"El precio actual del solana es: ${cripto_price}")
-    fecha = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Usa la fecha actual, 
+    # Obtener el precio del cardano en peso
+    cardano_price = data['cardano'][currency]
+    print(f"El precio actual del cardano es: ${cardano_price}")
+    fecha = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Usa la fecha actual, puedes cambiar esto según tus necesidades
 
-    insert_record(fecha, cripto_price)
+    insert_record(fecha, cardano_price)
     limite_inferior = valor_compra * (1 - (porcentaje_tolerancia_min / 100))
 
-    porcentaje_ganacia_perdida = 100 - ((valor_inicial_compra*100) / cripto_price)
+    porcentaje_ganacia_perdida = 100 - ((valor_inicial_compra*100) / cardano_price)
     monto_actual = valor_inversion_inicial * (1 + (porcentaje_ganacia_perdida/100) )
 
     print(f"porcentaje hoy: {porcentaje_ganacia_perdida} y monto actual {monto_actual}")
 
     #obtener los valores de la tendencia
-    arriba_del_promedio, abajo_del_promedio = calcula_tendencia_solana(450) #se puede parametrizar
+    arriba_del_promedio, abajo_del_promedio = calcula_tendencia_cardano(1000)
     tendencia = ""
     if arriba_del_promedio > abajo_del_promedio:
         tendencia = "al alza"
@@ -86,23 +86,25 @@ if response.status_code == 200:
     if(valor_inversion_inicial != 0) :
         if(monto_actual < valor_inversion_inicial) :
             #mandar correo
-            manda_mail("Alerta-solana: Perdida de dinero", f"porcentaje hoy: {porcentaje_ganacia_perdida}, monto invertido {valor_inversion_inicial} y monto actual {monto_actual}, porcentaje de registros arriba del promedio {arriba_del_promedio:.3f}% y porcentaje de registros abajo del promedio {abajo_del_promedio:.3f}%, la tendencia es {tendencia}")
+            manda_mail("cardano-Alerta: Perdida de dinero", f"porcentaje hoy: {porcentaje_ganacia_perdida}, monto invertido {valor_inversion_inicial} y monto actual {monto_actual}, porcentaje de registros arriba del promedio {arriba_del_promedio:.3f}% y porcentaje de registros abajo del promedio {abajo_del_promedio:.3f}%, la tendencia es {tendencia}")
         
-    if(porcentaje_ganacia_perdida > porcentaje_ganacia_ideal) :
+    if(porcentaje_ganacia_perdida > porcentaje_ganacia_ideal and valor_inversion_inicial != 0) :
       manda_mail("Atención: puede haber ganancias.", f"porcentaje hoy: {porcentaje_ganacia_perdida}, monto invertido {valor_inversion_inicial} y monto actual {monto_actual}")
     # Obtener el precio actual
-    precio_actual = cripto_price 
-    print(f"Precio actual de solana: {precio_actual}")
+    precio_actual = cardano_price 
+    print(f"Precio actual de cardano: {precio_actual}")
 
     # Verificar si debes comprar
 
     if precio_actual < limite_inferior:
 
-        print("Alerta-solana: El precio está por debajo del límite de tolerancia. Considera vender.")
-        manda_mail("COMPRA: El precio está por debajo del margen", f"Precio actual de solana: {precio_actual} y la tendecia de los ultimos 450 registros es {tendencia}%")
+        print("Alerta-cardano: El precio está por debajo del límite de tolerancia. Considera vender.")
+        manda_mail("COMPRA: El precio está por debajo del margen", f"Precio actual de cardano: {precio_actual} y la tendecia de los ultimos 450 registros es {tendencia}%")
     else:
-        print(f"El precio está dentro del rango de tolerancia. Puedes mantener tu inversión o comprar más.:{limite_inferior}")
-
+          print(f"El precio está dentro del rango de tolerancia. Puedes mantener tu inversión o comprar más.:{limite_inferior}")
+    # valor base y porcentaje 
+     # obtner la tendencia de los ultimos 1000 registros 
+     # logica de compra venta 
 
 else:
     print("Error al hacer la solicitud a la API")
